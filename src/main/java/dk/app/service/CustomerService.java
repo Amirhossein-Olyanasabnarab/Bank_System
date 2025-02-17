4008e75fc72ca7147486807ed489d8dbb697aa12
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-02-17 12:01:32 +0100

    CustomerService class

---
+++ src/main/java/dk/app/service/CustomerService.java
@@ -0,0 +1,22 @@
+package dk.app.service;
+
+import dk.app.model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerService {
+    private List<Customer> customers = new ArrayList<>();
+
+    //Singleton pattern
+    private static final CustomerService INSTANCE;
+    public static CustomerService getInstance() {
+        return INSTANCE;
+    }
+    static {
+        INSTANCE = new CustomerService();
+    }
+
+    private CustomerService() {}
+
+}
