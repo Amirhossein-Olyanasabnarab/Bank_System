6d34516ff614f3d0ea37ad32f1c8875d33410bb3
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-02-17 11:35:49 +0100

    LegalCustomer entity

---
+++ src/main/java/dk/app/model/LegalCustomer.java
@@ -0,0 +1,41 @@
+package dk.app.model;
+
+import dk.app.enums.CustomerType;
+
+public class LegalCustomer extends Customer{
+    private String companyName;
+    private String fax;
+
+    public LegalCustomer(String name, String phoneNumber, CustomerType customerType) {
+        super(name, phoneNumber, customerType);
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = capitalizeFirstLetter(companyName);
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    private String capitalizeFirstLetter(String str) {
+        return str.substring(0,1).toUpperCase()+str.substring(1);
+    }
+
+    @Override
+    public String toString() {
+        return "LegalCustomer{ " +
+                super.toString() +
+                ", companyName='" + companyName + '\'' +
+                ", fax='" + fax + '\'' +
+                '}';
+    }
+}
