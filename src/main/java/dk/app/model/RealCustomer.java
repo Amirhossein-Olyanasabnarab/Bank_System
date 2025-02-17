d44c1b67445a8f540f00a65aee6f387c9ed9b3d7
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-02-17 11:32:47 +0100

    RealCustomer entity

---
+++ src/main/java/dk/app/model/RealCustomer.java
@@ -0,0 +1,41 @@
+package dk.app.model;
+
+import dk.app.enums.CustomerType;
+
+public class RealCustomer extends Customer {
+
+    private String family;
+    private String mobilePhoneNumber;
+
+    public RealCustomer(String name, String phoneNumber, CustomerType customerType) {
+        super(name, phoneNumber, customerType);
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = capitalizeFirstLetter(family);
+    }
+
+    public String getMobilePhoneNumber() {
+        return mobilePhoneNumber;
+    }
+
+    public void setMobilePhoneNumber(String mobilePhoneNumber) {
+        this.mobilePhoneNumber = mobilePhoneNumber;
+    }
+    private String capitalizeFirstLetter(String str) {
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
+    @Override
+    public String toString() {
+        return "RealCustomer{ " +
+                super.toString() +
+                ", family='" + family + '\'' +
+                ", mobilePhoneNumber='" + mobilePhoneNumber + '\'' +
+                '}';
+    }
+}
