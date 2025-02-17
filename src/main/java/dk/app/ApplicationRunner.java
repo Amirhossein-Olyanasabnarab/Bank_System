2d94b63f0d0851f9521af7eef4ac9dc07d62b8b6
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-02-17 11:52:25 +0100

    added ConsoleUI and startMenu

--- src/main/java/dk/app/ApplicationRunner.java
+++ src/main/java/dk/app/ApplicationRunner.java
@@ -1,7 +1,11 @@
 package dk.app;
 
+import dk.app.view.ConsoleUI;
+
 public class ApplicationRunner {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try(ConsoleUI consoleUI = new ConsoleUI()) {
+            consoleUI.startMenu();
+        }
     }
 }
--- src/main/java/dk/app/view/ConsoleUI.java
+++ src/main/java/dk/app/view/ConsoleUI.java
@@ -8,11 +8,35 @@ public class ConsoleUI implements AutoCloseable{
         scanner = new Scanner(System.in);
     }
 
-    private void startMenu(){
+    public void startMenu(){
+        int choice;
+        do {
+            printMenu();
+            choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    System.out.println("Exit");
+                    break;
+                case 1:
+                    System.out.println("Add new customer");
+                    break;
+                case 2:
+                    System.out.println("Show all customers");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+        }while (choice != 0);
+    }
+
+    private void printMenu(){
         System.out.println("Please choose one of the following options:");
         System.out.println("0. Exit");
         System.out.println("1. Add new customers");
         System.out.println("2. Show all customers");
+        System.out.println("Please choose one of the  above options:");
     }
     @Override
     public void close() {
