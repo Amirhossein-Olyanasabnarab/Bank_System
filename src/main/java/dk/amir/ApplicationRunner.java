7c81418c32078fc8205967afa8d7fa0a06478e03
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-03-06 11:55:48 +0100

    Application started. Initializing ConsoleUI... If an error occurs, it will be logged.

--- src/main/java/dk/amir/ApplicationRunner.java
+++ src/main/java/dk/amir/ApplicationRunner.java
@@ -2,16 +2,15 @@ package dk.amir;
 
 import dk.amir.view.ConsoleUI;
 
-/**
- * Hello world!
- *
- */
+
 public class ApplicationRunner
 {
     public static void main( String[] args )
     {
         try(ConsoleUI consoleUI = new ConsoleUI()){
             consoleUI.startMenu();
+        }catch (Throwable ex){
+            System.out.println("An error occurred while running the application.");
         }
     }
 }
